4be79b5f11581f829907a24097eafc6b7c4df527
unknown <benjamindrews@gmail.com>
2021-09-15 22:26:50 -0700

    Added unlocked camera movement with middle mouse

---
+++ Sedah/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public bool lockedMode = true;
+    public Vector3 forward, right;
+    public float cameraSpeed = 5f;
+    Vector3 buttonDownMousePosition;
+
+    void Start()
+    {
+        forward = Camera.main.transform.forward;
+        forward.y = 0;
+        forward = Vector3.Normalize(forward);
+        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            lockedMode = !lockedMode;
+        }
+
+        if (!lockedMode)
+        {
+            if (Input.GetMouseButtonDown(2))
+            {
+                buttonDownMousePosition = Input.mousePosition;
+            }
+        } 
+    }
+
+    private void FixedUpdate()
+    {
+        if (!lockedMode)
+        {
+            if (Input.GetMouseButton(2))
+            {
+                Vector3 dir = Input.mousePosition - buttonDownMousePosition;
+                Vector3 rightMovement = right * cameraSpeed * Time.deltaTime * dir.x;
+                Vector3 forwardMovement = forward * cameraSpeed * Time.deltaTime * dir.y;
+
+                this.transform.position += rightMovement + forwardMovement;
+            }
+        }
+    }
+}
+
