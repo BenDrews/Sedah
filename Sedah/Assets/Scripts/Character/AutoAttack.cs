a991a65ca5e976392f691b8431f4639a04469104
unknown <benjamindrews@gmail.com>
2021-09-25 02:07:28 -0700

    Abstracted autoattack subclass out of playerautoattack

---
+++ Sedah/Assets/Scripts/Character/AutoAttack.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sedah;
+using Mirror;
+
+[RequireComponent(typeof(CharacterObject))]
+[RequireComponent(typeof(EntityStateMachine))]
+public abstract class AutoAttack : NetworkBehaviour
+{
+    private CharacterObject character;
+    public GameObject target;
+    [SyncVar]
+    private double timeLastAttacked;
+
+    [Command]
+    protected void CmdSetAutoAttackState(GameObject target)
+    {
+        EntityStateMachine stateMachine = GetComponent<EntityStateMachine>();
+        stateMachine.SetNextState(new AutoAttackingState(stateMachine));
+        this.target = target;
+    }
+
+    protected void AttemptAutoAttack(GameObject target)
+    {
+        EntityStateMachine stateMachine = GetComponent<EntityStateMachine>();
+        float attackSpeed = character.GetStatValue(StatType.AttackSpeed);
+        double timeForNextAttack = double.IsNegativeInfinity(timeLastAttacked) ? double.NegativeInfinity : NetworkTime.time + (1.0 / attackSpeed);
+        if (stateMachine.GetState().type == EntityStateType.AutoAttacking)
+        {
+            // Check if target is in range
+            float dist = Vector3.Distance(transform.position, target.transform.position);
+            if (dist <= this.character.GetStatValue(StatType.AttackRange))
+            {
+                // Stop moving if target is in range
+                this.GetComponent<PlayerMovement>()?.CmdMove(transform.position);
+                if (timeForNextAttack < NetworkTime.time)
+                {
+                    // If our attack is off cooldown, fire an attack
+                    FireAutoAttack(target);
+                }
+            }
+            else
+            {
+                this.GetComponent<PlayerMovement>()?.CmdMove(target.transform.position);
+            }
+        }
+    }
+
+    protected void FireAutoAttack(GameObject target)
+    {
+        float damage = character.GetStatValue(StatType.AttackDamage);
+        float attackSpeed = character.GetStatValue(StatType.AttackSpeed);
+        DamageInfo dmgInfo = new DamageInfo(damage, gameObject, target.transform.position, DamageType.Physical);
+        target.GetComponent<Health>().TakeDamage(dmgInfo);
+        timeLastAttacked = NetworkTime.time;
+    }
+
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        this.character = GetComponent<CharacterObject>();
+        this.timeLastAttacked = double.NegativeInfinity;
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if (hasAuthority)
+        {
+            if (isServer)
+            {
+                AttemptAutoAttack(target);
+            }
+        }
+    }
+}
